5d65c28b660576272b4f957ec15764dce4eaaa0b
Elisse <elisse_arias@hotmail.com>
2022-08-14 01:13:30 -0500

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpack = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    output:{
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {  
+                            esModule: false,
+                            name: 'assets/img/[name].[ext]',
+                        }
+                    }
+                ],
+            }
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ],
+}
